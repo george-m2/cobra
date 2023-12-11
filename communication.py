f664e95f71e8a0724a189f04d8c32157a75574b4
jahzera <george.m20420@gmail.com>
2023-12-11 11:36:19 +0000

    communication.py now awaits Unity move response

--- communication.py
+++ communication.py
@@ -1,15 +1,23 @@
-import time
 import zmq
 
+def process_pgn(pgn):
+    # Placeholder for PGN processing and best move finding logic
+    # minimax()
+    return "e2e4"
+
+
+# Setting up the ZeroMQ context and socket
 context = zmq.Context()
 socket = context.socket(zmq.REP)  # REP - reply/RequestSocket
 socket.bind("tcp://*:5555")
 
 while True:
-    #  wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
-    # time delay - depends on length of tree search
-    time.sleep(1)
-    #  Send reply back to Chess.NET
-    socket.send("test".encode('utf-8'))
+    # Wait for the next request from the C# client
+    pgn_message = socket.recv().decode('utf-8')
+    print("Received PGN: %s" % pgn_message)
+
+    # Process the PGN and find the best move
+    best_move = process_pgn(pgn_message)
+
+    # Send the best move back to the C# client
+    socket.send(best_move.encode('utf-8'))
